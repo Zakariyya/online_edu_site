d4de255e64d3c4ee321d07b54d3a4e3d956212a2
anan <rabbittucute@gmail.com>
2020-05-18 18:49:48 +0800

    统一日志处理
    1. 删除 application-dev.yml 中的配置，因为会跟 logback 冲突
    # mybatis-plus:
    #   configuration:
    #     log-impl: org.apache.ibatis.logging.stdout.StdOutImpl
    
    # 日志
    # logging:
    #   level:
    #     root: info  # OFF, FATAL, ERROR, WARN, INFO, DEBUG, ALL
    
    2. 添加 logback-spring.xml 可以指定日志级别和生成日志目录
    
    3. @Sl4j 的 log.error() 的内容会写入到对应级别的日志文件下的
    
    4. common/base/exception/ExceptionUtil 将详细的异常信息取出，封装

--- common/base/src/main/java/com/anan/common/base/exception/GlobalException.java
+++ common/base/src/main/java/com/anan/common/base/exception/GlobalException.java
@@ -1,6 +1,7 @@
 package com.anan.common.base.exception;
 
 import com.anan.common.base.utils.R;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @Date: 2020/5/18 16:35
  */
 @ControllerAdvice
+@Slf4j
 public class GlobalException {
 
   /**
@@ -42,6 +44,12 @@ public class GlobalException {
   @ResponseBody  // 为了能返回数据
   public R errorAnan(AnanException e){
     e.printStackTrace();
+
+    // error的话就会写到 log.error.log 里面（配合 logback 使用）
+    // log.info(), 就是写到 log.info.log 文件里面
+    log.error(e.getMessage());
+    log.error(ExceptionUtil.getMessage(e)); // ExceptionUtil 里面将堆栈异常信息打印出来，比直接 e.getMessage() 更加详细
+
     return R.success().code(e.getCode()).msg(e.getMsg());
   }
 
